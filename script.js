216271a856642f6eb532b98f26a97c0ffcb2ba39
digitallegaldesk <licitcrest@gmail.com>
2025-07-28 22:36:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const yayChatBtn = document.getElementById('yay-chatbot');
+const yayChatWindow = document.getElementById('yay-chat-window');
 
+yayChatBtn.addEventListener('click', () => {
+  if (yayChatWindow.style.display === 'none' || yayChatWindow.style.display === '') {
+    yayChatWindow.style.display = 'block';
+  } else {
+    yayChatWindow.style.display = 'none';
+  }
+});
